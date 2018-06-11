115d443d014e991dba61ce49e9d621d0777a0372
Vignesh M <mvigneshtuty@gmail.com>
2018-06-12 00:35:00 +0530

    unit test cases addition

--- rayfocus-tasklet.api/src/test/java/com/rayfocus/api/tasklet/TaskletServiceTests.java
+++ rayfocus-tasklet.api/src/test/java/com/rayfocus/api/tasklet/TaskletServiceTests.java
@@ -19,6 +19,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.amazonaws.services.dynamodbv2.util.TableUtils.TableNeverTransitionedToStateException;
 import com.rayfocus.api.tasklet.http.HttpResponse;
 import com.rayfocus.api.tasklet.model.SubTask;
 import com.rayfocus.api.tasklet.model.Task;
@@ -27,7 +29,7 @@ import com.rayfocus.api.tasklet.service.TaskletService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public final class TaskletRepositoryTests {
+public final class TaskletServiceTests {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -36,33 +38,49 @@ public final class TaskletRepositoryTests {
 
 	@Autowired
 	private TaskletService taskletService;
+	
+	private static boolean taskTableExist = false;
 
 	@Before
-	public void setUp() throws InterruptedException {
-		boolean isTableActive = false;
-		amazonDynamoDB.createTable(new DynamoDBMapper(amazonDynamoDB).generateCreateTableRequest(Task.class)
-				.withProvisionedThroughput(new ProvisionedThroughput(2L, 2L)));
-		logger.info("waiting for Tasks table to be Active...");
-		while (!isTableActive) {
-			if (amazonDynamoDB.describeTable("Tasks").getTable().getTableStatus().equals("ACTIVE")) {
-				isTableActive = true;
-				logger.info("~~~ Tasks table is Active now ~~~");
+	public void setUp() throws TableNeverTransitionedToStateException, InterruptedException {
+		if( ! taskTableExist ) {
+			if(TableUtils.createTableIfNotExists(amazonDynamoDB, new DynamoDBMapper(amazonDynamoDB).generateCreateTableRequest(Task.class)
+					.withProvisionedThroughput(new ProvisionedThroughput(2L, 2L)))) {
+				logger.info("~~~ Waiting for Tasks table to be active ~~~");
+				TableUtils.waitUntilActive(amazonDynamoDB, "Tasks");
 			}
+			else {
+				logger.info("~~~ Tasks table already exists ~~~");
+			}
+			taskTableExist = true;
 		}
 	}
 
 	@Test
 	public void testSaveTask() {
-		logger.info("Saving the taskable Items");
-		
+		logger.info("Saving the taskable items");
+
 		List<SubTask> subTasks = new ArrayList<SubTask>(
 				Arrays.asList(new SubTask("Vegetables", "INCOMPLETE"), new SubTask("Fruits", "COMPLETE")));
-		Task newTask = new Task("145", "Buy Groceries", "Active", subTasks, "VigneshM@taskletAPI.com", "02-JUN-18");
+		Task newTask = new Task("456", "Buy Groceries", "Active", subTasks, "users@taskletAPI.com", "11-JUN-2018");
 
 		ResponseEntity<HttpResponse> response = taskletService.saveTask(newTask);
-		
+
 		assertTrue("Task saved successfully.", response.getBody().getStatusCode() == "200");
-		assertTrue("Task Id matches", ((TaskMetaData) response.getBody().getResponseData()).getTaskId() == "145");
+		assertTrue("Task Id matches", ((TaskMetaData) response.getBody().getResponseData()).getTaskId() == "456");
 		logger.info("Save task done");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetTaskByUserId() {
+		logger.info("Fetching the task items");
+
+		ResponseEntity<HttpResponse> response = taskletService.getTaskByUserId("users@taskletAPI.com");
+
+		assertTrue("Task fetched successfully.", response.getBody().getStatusCode() == "200");
+		assertTrue("User Id matches", ((List<Task>) response.getBody().getResponseData()).get(0).getUserId()
+				.equals("users@taskletAPI.com"));
+		logger.info("Get task done");
 	}
 }
