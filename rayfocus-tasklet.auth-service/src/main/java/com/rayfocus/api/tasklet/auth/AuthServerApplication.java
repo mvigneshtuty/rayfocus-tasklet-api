83db754833e591f03d30f6b40ff02d9a0966256d
Vignesh M <mvigneshtuty@gmail.com>
2018-06-26 16:47:05 +0530

    Adding ResourceServer annotation

--- rayfocus-tasklet.auth-service/src/main/java/com/rayfocus/api/tasklet/auth/AuthServerApplication.java
+++ rayfocus-tasklet.auth-service/src/main/java/com/rayfocus/api/tasklet/auth/AuthServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+//@EnableResourceServer
 @EnableAuthorizationServer
 public class AuthServerApplication {
 	public static void main(String[] args) {
