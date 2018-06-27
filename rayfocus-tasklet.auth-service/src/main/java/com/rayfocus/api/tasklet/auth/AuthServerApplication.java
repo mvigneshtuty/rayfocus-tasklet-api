39906f6da97e900fa8a6d761cb7c269320cefdc0
Vignesh M <mvigneshtuty@gmail.com>
2018-06-27 15:29:16 +0530

    shaping up the OAuth2 Authorization server

--- rayfocus-tasklet.auth-service/src/main/java/com/rayfocus/api/tasklet/auth/AuthServerApplication.java
+++ rayfocus-tasklet.auth-service/src/main/java/com/rayfocus/api/tasklet/auth/AuthServerApplication.java
@@ -3,9 +3,10 @@ package com.rayfocus.api.tasklet.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-//@EnableResourceServer
+@EnableResourceServer
 @EnableAuthorizationServer
 public class AuthServerApplication {
 	public static void main(String[] args) {
