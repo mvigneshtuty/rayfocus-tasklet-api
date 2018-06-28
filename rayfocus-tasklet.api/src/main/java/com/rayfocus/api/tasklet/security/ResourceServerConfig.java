46d9e6c246b29e6ebb0bc3b0ab36f2a7a7173232
Vignesh M <mvigneshtuty>
2018-06-28 17:32:39 +0530

    protecting the resource using OAuth2

---
+++ rayfocus-tasklet.api/src/main/java/com/rayfocus/api/tasklet/security/ResourceServerConfig.java
@@ -0,0 +1,18 @@
+package com.rayfocus.api.tasklet.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig  extends ResourceServerConfigurerAdapter{
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// to allow all the authorized requests
+		http.authorizeRequests().anyRequest().authenticated();
+		
+	}
+
+
+}
